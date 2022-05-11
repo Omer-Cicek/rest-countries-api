8d1d7c58f6eea3de3704235b80f37206203eca0a
Omer-Cicek <omercicek160408@gmail.com>
2022-05-11 19:01:10 +0300

    loading spinner added

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,25 +1,30 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import {
-  InputGroup,
-  FormControl,
-  SplitButton,
-  Dropdown,
-  Table,
-} from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 
 const CountriesList = () => {
-  const [posts, setPosts] = useState([]);
-  //   const [filteredPosts, setFilteredPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [posts, setPosts] = useState(null);
+  const [filteredPosts, setFilteredPosts] = useState(null);
   const [input, setInput] = useState('');
   const [option, setOption] = useState('');
+
+  let newArr = [];
 
   const baseURL = 'https://restcountries.com/v2/all';
 
   useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPosts(response.data);
-    });
+    setIsLoading(true);
+    axios
+      .get(baseURL)
+      .then((response) => {
+        setPosts(response.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
   }, []);
 
   const handleInput = (e) => {
@@ -30,30 +35,68 @@ const CountriesList = () => {
     setOption(e.target.value);
   };
 
+  const resetHandler = () => {
+    setFilteredPosts(null);
+    setInput('');
+    setOption('');
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(option, input);
+
+    if (option === 'searchWithCapital') {
+      newArr = posts.filter((post) =>
+        post?.capital?.toLowerCase().includes(input.toLowerCase())
+      );
+      console.log(newArr);
+      setFilteredPosts(newArr);
+    } else {
+      newArr = posts.filter((post) => {
+        let { name, capital, region } = post;
+        return (
+          name?.toLowerCase().includes(input.toLowerCase()) ||
+          capital?.toLowerCase().includes(input.toLowerCase()) ||
+          region?.toLowerCase().includes(input.toLowerCase())
+        );
+      });
+      console.log(newArr);
+      setFilteredPosts(newArr);
+    }
   };
 
   return (
     <>
       {/* Search bar  */}
-      <form className="d-flex flex-row " onSubmit={(e) => handleSubmit(e)}>
-        <input
-          type="search"
-          className="form-control w-50"
-          id="search"
-          placeholder="Search"
-          onChange={(e) => handleInput(e)}
-        />
-        <select className="form-select w-25" onChange={(e) => handleOptions(e)}>
-          <option value="commonSearch">Common Search</option>
-          <option value="searchWithCapital">Search With Capital</option>
-        </select>
-        <button type="submit" className="btn btn-secondary">
-          Search
+      <form
+        className="d-flex flex-row justify-content-evenly "
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <div className="d-flex justify-content-center w-75 gap-2">
+          <input
+            type="search"
+            className="form-control w-50"
+            id="search"
+            placeholder="Search"
+            onChange={(e) => handleInput(e)}
+          />
+          <select
+            className="form-select w-25"
+            onChange={(e) => handleOptions(e)}
+          >
+            <option value="commonSearch">Common Search</option>
+            <option value="searchWithCapital">Search With Capital</option>
+          </select>
+
+          <button type="submit" className="btn btn-secondary">
+            Search
+          </button>
+        </div>
+        <button type="button" className="btn btn-danger" onClick={resetHandler}>
+          Reset
         </button>
       </form>
+
       {/* Table  */}
       <Table className="table table-striped table-hover">
         {/* table titles  */}
@@ -68,8 +111,31 @@ const CountriesList = () => {
         </thead>
         {/* table body-displaying countries and infos conditionally */}
         <tbody>
-          {posts
-            ? posts.map((post, index) => {
+          {posts &&
+            (filteredPosts ? (
+              filteredPosts.length > 0 ? (
+                filteredPosts.map((post, index) => {
+                  return (
+                    <tr key={post.numericCode}>
+                      <td>{index + 1}</td>
+                      <td>{post?.name}</td>
+                      <td>{post?.capital}</td>
+                      <td>{post?.region}</td>
+                      <td>
+                        <img
+                          src={post?.flags.svg}
+                          alt="countryFlags"
+                          style={{ width: '50px' }}
+                        />
+                      </td>
+                    </tr>
+                  );
+                })
+              ) : (
+                <p>Veri Bulunamadı</p>
+              )
+            ) : (
+              posts.map((post, index) => {
                 return (
                   <tr key={post.numericCode}>
                     <td>{index + 1}</td>
@@ -86,9 +152,15 @@ const CountriesList = () => {
                   </tr>
                 );
               })
-            : null}
+            ))}
         </tbody>
       </Table>
+      {/* spinner when data loading */}
+      {isLoading && (
+        <div className="spinner-grow text-dark text-center mt-5" role="status">
+          <span className="visually-hidden text-center">Loading...</span>
+        </div>
+      )}
     </>
   );
 };
